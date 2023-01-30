ed12182327a2a569a5f08d52fd553b7424275683
voyager <cdspacenoob@gmail.com>
2023-01-30 16:18:59 +0900

    portal 및 모달 구현

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Fragment } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 const App = () => {
   return (
     <Fragment>
+      <Cart />
       <Header />
       {/* main은 기본 HTML5 요소 */}
       <main>
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+// 장바구니 창. 모달로 구현한다. 
+import Modal from '../UI/Modal';
+
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+    const cartItems = <ul>{
+        [
+            { id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }
+        ].map((item) => (item.name))
+    }</ul>;
+    return (
+        <Modal>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </Modal>
+    );
+};
+
+export default Cart;
