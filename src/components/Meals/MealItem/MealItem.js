068f1c4371bf31e3c86fc7b2946d1ff2c4a248f1
voyager <cdspacenoob@gmail.com>
2023-01-31 15:21:44 +0900

    입력받기, context 사용 준비

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,24 @@
+import { useContext } from 'react';
 import MealItemForm from './MealItemForm';
 
+import CartContext from '../../context/cart-context';
 import classes from './MealItem.module.css';
 
+
 const MealItem = (props) => {
+    const cartContext = useContext(CartContext);
+
     // toFixed()는 소수점 아래 몇 자리까지 표시할 것인지 정할 수 있게 해준다.
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = (amount) => {
+        cartContext.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
 
     return (
         <li className={classes.meal}>
@@ -14,7 +28,7 @@ const MealItem = (props) => {
             <div className={classes.description}>{props.description}</div>
             <div className={classes.price}>{price}</div>
             <div>
-                <MealItemForm />
+                <MealItemForm onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
